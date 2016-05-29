640490231417fe2df6a0f4e7425103a11696b8d5
Megan Martinson <meg.martinson@gmail.com>
2016-05-29 18:54:21 -0500

    upload it to git

---
+++ javascriptin.js
@@ -0,0 +1,9 @@
+var username= prompt("Hey friend! What is your name?", "Gucci Mane?")
+var output= document.getElementById('greeting')
+
+function workin(username) {
+  console.log("Hey there, "+ username + "!");
+  console.log("This totally works!");
+}
+
+var output = workin(username)
